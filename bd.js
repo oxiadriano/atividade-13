03c2f8d785fec16d12e33bd01bf055913a8dde69
oxiadriano <164542384+oxiadriano@users.noreply.github.com>
2025-02-21 17:45:53 +0000

    dando certo

--- bd.js
+++ bd.js
@@ -38,4 +38,12 @@ async function insertUsuario(data) {
 }
 
 //bd.js
-export { selectUsuarios, selectUsuario, insertUsuario };
+async function deleteUsuario(id) {
+  const client = await connect();
+  const query = "DELETE FROM usuario WHERE id = $1";
+  await client.query(query, [id]);
+  client.release();
+}
+
+
+export { selectUsuarios, selectUsuario, insertUsuario, deleteUsuario };
