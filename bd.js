5399889c124ceca5e43cd819e92bc199caaa8fd0
oxiadriano <164542384+oxiadriano@users.noreply.github.com>
2025-02-21 17:03:47 +0000

    aeeeeeee

---
+++ bd.js
@@ -0,0 +1,21 @@
+//bd.js
+import pkg from "pg";
+const { Pool } = pkg;
+
+//bd.js
+async function connect() { // depois mudar o nome da função pra não confundir com o método connet do pool
+    const pool = new Pool({
+      connectionString: process.env.URL_BD,
+    });
+    return pool.connect();
+  }
+
+  //bd.js
+async function selectUsuarios() {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM usuario");
+    client.release();
+    return res.rows;
+  }
+
+  export { selectUsuarios };
--- index.js
+++ index.js
@@ -1,12 +1,28 @@
-import express from "express";      // Requisição do pacote do express
+import dotenv from "dotenv";
+import { selectUsuarios } from "./bd.js";
+import express from "express"
+dotenv.config();
+
+;      // Requisição do pacote do express
 const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
+const port = 3030;                  // Define a porta
 
 app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
   console.log("Rota GET/ solicitada");
   res.json({
     nome: "Adriano MIranda",      // Substitua pelo seu nome
   });
+});
+
+//index.js
+app.get("/usuario", async (req, res) => {
+  console.log("Rota GET/usuario solicitada");
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
 });
 
 app.listen(port, () => {            // Um socket para "escutar" as requisições
